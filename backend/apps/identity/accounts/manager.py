2ec9c65f5dc1226fc914d038905de22649692c57
husniddin <alikkhan1680@gmail.com>
2026-02-01 23:12:02 +0500

    faillar strukturasi va docker sozlandi va commond manager to'gilandai

--- backend/apps/identity/accounts/manager.py
+++ backend/apps/identity/accounts/manager.py
@@ -1,16 +1,36 @@
 from django.contrib.auth.models import BaseUserManager
 
-class UserManager(BaseUserManager):
-    def create_user_with_role(self, **extra_fields):
-        from .models import CustomUser
 
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError("Username is required")
+
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user_with_role(self, **extra_fields):
         if self.model.objects.count() == 0:
             extra_fields["user_role"] = "account_owner"
         else:
             extra_fields.setdefault("user_role", "employee")
 
+        password = extra_fields.pop("password", None)
+        username = extra_fields.get("username")
 
-        user = self.model(**extra_fields)
-        user.set_password(extra_fields.get("password"))
-        user.save(using=self._db)
-        return user
+        return self.create_user(username=username, password=password, **extra_fields)
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+
+        return self.create_user(username=username, password=password, **extra_fields)
